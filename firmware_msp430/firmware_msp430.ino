48abebd8e35af1c9f6221d98d2bf6fefdb1e43e5
elliotelm <elliot.elm@gmail.com>
2015-04-29 13:26:19 -0700

    SPI slave working!

--- firmware_msp430/firmware_msp430.ino
+++ firmware_msp430/firmware_msp430.ino
@@ -25,14 +25,15 @@
 #define CC_POL P2_1         // input pin 2.1 FROM CC
 #define CC_INT P2_2         // input pin 2.2 FROM CC
 
-#define Battery_Max_Charge 64000     // 
+#define Battery_Max_Charge 100       // 
 #define Battery_Min_Charge 0         // 
 #define Battery_BB_Min 10            // lowest battery percentage that BeagleBone can be turned on at
 #define Battery_Low_Threshold 5      // absolute lowest battery percentage allowed 
 #define Charge_Step 1                // increment for when CC shows battery charged
 #define Discharge_Step 1             // increment for when CC shows battery discharged
 
-volatile double CC_Fire = 500;                      // battery charge 
+volatile uint8_t Battery_Step = 193;      // TODO fix this // 193 interrupts from coulomb counter is 1% of battery charge
+volatile uint8_t CC_Fire = 1;                  // battery charge interrupt tracking
 volatile uint8_t Battery_Level = 100;               // battery charge percentage
 volatile uint8_t BB_Power = FALSE;                  // treat as boolean, TRUE = BeagleBone is ON, FALSE = BeagleBone is OFF
 volatile uint8_t CC_Shutdown = FALSE;               // treat as boolean, TRUE = Shutdown coulomb counter, FALSE = Turn coulomb counter back on
@@ -58,9 +59,9 @@ void setup()
   
   spi_init();                                        // initialize spi
  
-  //P2IE |= BIT2;                                     // P2.2 interrupt enabled, CC_INT
-  //P2IES |= BIT2;                                  //P1IES  // P2.2 Hi/lo edge (fires on hi/lo transition)
-  //P2IFG &= ~BIT2;                                 //P1IFG // P2.2 IFG cleared, clear the flag
+  P2IE |= BIT2;                                     // P2.2 interrupt enabled, CC_INT
+  P2IES |= BIT2;                                  //P1IES  // P2.2 Hi/lo edge (fires on hi/lo transition)
+  P2IFG &= ~BIT2;                                 //P1IFG // P2.2 IFG cleared, clear the flag
 }
 
 //*****************************************************************************************
@@ -87,7 +88,7 @@ void spi_init(void)
 UCA0CTL1 = UCSWRST;                                 // hold in reset
                                                     // 4-pin, 8-bit SPI slave, STE (slave enable) active high
 //UCA0CTL0 |= UCCKPH + UCMSB + UCMODE_1 + UCSYNC;     // Sync. Mode: Clock Phase, Async. Mode: MSB first  0:LSB / 1:MSB, Sync. Mode: USCI Mode: 1, Sync-Mode  0:UART-Mode / 1:SPI-Mode
-UCA0CTL0 |= UCCKPH + UCMSB + UCSYNC;    
+UCA0CTL0 |= UCCKPH + UCMSB + UCSYNC;     
 // UCxCLK is always used in slave mode
 UCA0CTL1 &= ~UCSWRST;                               // release reset
 IE2 |= UCA0RXIE;                                    // USCI_A0 receive interrupt enable
@@ -177,7 +178,6 @@ __interrupt void Timer_A (void)
   
   if(((ISRA0count % 59) == 0) && (ISRA0count > 1))                     // if 1 min. has passed 
   {
-   // Battery_Level = 9;                                                // simulate battery went low
     if ((Battery_Level > Battery_Low_Threshold) && (CC_Shutdown))     // CC is OFF and Battery is above absolute minimum
       {
         CC_Shutdown = FALSE;                                          // coulomb counter doesn't need to be turned off
@@ -196,7 +196,8 @@ __interrupt void Timer_A (void)
       }
   digitalWrite(BB_GP_ISR, LOW);                                       // turn off outputs to BB for safety
   BB_Pow_Check();                                                     // check to see if the BB is On or Off
-  UCA0TXBUF = 0x0A;                                      // TEST TEST // keep the battery level updated in the SPI data register 
+  // UCA0TXBUF = Battery_Level;                                      // TEST TEST // keep the battery level updated in the SPI data register 
+ 
  // TODO calculation for the situation in which battery went low, coulomb counter is off (need to manually adjust battery level for ?? hour(s) of charge)
 }//timer/counter A0 ISR
 
@@ -207,25 +208,49 @@ __interrupt void Timer_A (void)
 // direction.
 // 3600 mAh battery 
 //*****************************************************************************************
-/*
+
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {   
-  // TODO update the battery charge variables here
+ uint8_t blah = 0;
+ blah ^= 1;
+ if(blah)
+ {
+ pinMode(P1_0, OUTPUT);
+ digitalWrite(P1_0, HIGH);
+ }
+ else
+ pinMode(P1_0, OUTPUT);
+ digitalWrite(P1_0, LOW);
+ 
   if (CC_POL)                                                      // coulomb counter indicates charging
     {
-      CC_Fire += Charge_Step;                                      // increment the battery charge
-      // Battery_Level = ??                                        // calculate the battery charge percentage
-    }
+      CC_Fire += Charge_Step;                                      // increment the CC count positive (charging)
+      if (CC_Fire > 193)
+      {
+        CC_Fire = 0;
+        if(Battery_Level < 100)                                    // battery level should not report above 100%
+        {
+          Battery_Level += Charge_Step;                            // calculate the battery charge percentage 
+        }
+      }
+     }
   else
     {
-      CC_Fire -= Discharge_Step;                                   // decrement the battery charge
-      // Battery_Level = ??                                        // calculate the battery charge percentage
+      CC_Fire -= Discharge_Step;                                   // decrement the CC count negative (discharging)
+      if (CC_Fire < 1)
+      {
+        CC_Fire = 193;
+        if(Battery_Level > 0)                                      // battery level should not report below 0%
+        {
+          Battery_Level -= Discharge_Step;                         // calculate the battery charge percentage
+        } 
+      }
     }
-   // remove for TEST UCA0TXBUF = Battery_Level;                                      // keep the battery level updated in the SPI data register
+   UCA0TXBUF = Battery_Level;                                      // keep the battery level updated in the SPI data register
    P2IFG &= ~BIT2;                                                 // P2.2 IFG (interrupt flag) cleared 
 }// PORT2 ISR
-*/
+
 //*****************************************************************************************
 // SPI ISR
 // receive 
@@ -235,6 +260,7 @@ __interrupt void Port_2(void)
 __interrupt void USCI0RX_ISR (void)
 {
  //UCA0TXBUF is read by the BeagleBone, the interrupt is cleared
+ /*
  uint8_t blah = 0;
  blah ^= 1;
  if(blah)
@@ -245,7 +271,10 @@ __interrupt void USCI0RX_ISR (void)
  else
   pinMode(P1_0, OUTPUT);
  digitalWrite(P1_0, LOW);
- UCA0TXBUF = 0x0A;
+ //UCA0TXBUF = 0x0A;
+ uint16_t locRxData =0;
+ locRxData = UCA0RXBUF;
+ */
 //IFG2 &= ~UCB0RXIFG;             // Clear USCI_B0 RX int flag 
 }
 // transfer should not be necessary for slave
